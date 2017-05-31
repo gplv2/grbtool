148b8262c3c404b6b263be3f4cfcfd1e373ba6ce
Glenn Plas <glenn@byte-consult.be>
2017-05-31 18:31:43 +0000

    add agiv stuff

---
+++ public/js/agivlayers.js
@@ -0,0 +1,362 @@
+/*jslint node: true, maxerr: 50, indent: 4 */
+"use strict";
+var agiv_layer;
+var isagivup = null;
+
+var webmercator = new OpenLayers.Projection( "EPSG:3857" );
+var geodetic = new OpenLayers.Projection( "EPSG:4326" );
+var mercator = new OpenLayers.Projection( "EPSG:900913" ); // to Spherical Mercator Projection
+var lambert = new OpenLayers.Projection( "EPSG:31370" ); // to Spherical Mercator Projection
+
+$( document ).ready( function() {
+
+
+    var refresh = new OpenLayers.Strategy.Refresh( {
+        force: true,
+        active: true
+    } );
+
+    var geojson_format = new OpenLayers.Format.GeoJSON( {
+        extractAttributes: true,
+        internalProjection: map.getProjectionObject(),
+        externalProjection: geodetic
+    } );
+
+    agiv_layer = new OpenLayers.Layer.Vector( 'CRAB - Address Source (aptum)', {
+        //styleMap: overpass_style,
+        styleMap: vectorlayer_style,
+        format: geojson_format,
+        strategies: [ boxStrategy, refresh ],
+        maxScale: 420,
+        //minScale: 6772,
+        minScale: 3000,
+        //maxResolution: map.getResolutionForZoom(15),
+        //zoomOffset: 9, resolutions: [152.87405654907226, 76.43702827453613, 38.218514137268066, 19.109257068634033, 9.554628534317017, 4.777314267158508, 2.388657133579254, 1.194328566789627, 0.5971642833948135],
+        //zoomOffset: 10, resolutions: [76.43702827453613, 38.218514137268066, 19.109257068634033, 9.554628534317017, 4.777314267158508, 2.388657133579254, 1.194328566789627, 0.5971642833948135],
+        protocol: new OpenLayers.Protocol.HTTP( {
+            url: "https://agivdata.grbosm.site/grb?",
+            format: geojson_format
+        } ),
+
+        projection: mercator,
+        //displayProjection: mercator
+        isBaseLayer: false
+    } );
+    //map.setLayerIndex(grb_wms, 1);
+
+    var highlightagiv = new OpenLayers.Control.SelectFeature( agiv_layer, {
+        hover: true,
+        highlightOnly: true,
+        //autoActivate:true,
+        toggle: false,
+        renderIntent: "temporary",
+        eventListeners: {
+            // featurehighlighted: report
+            featurehighlighted: onFeatureSelect,
+            featureunhighlighted: onFeatureUnselect
+        }
+    } );
+
+    // create selection lists
+    agiv_layer.events.register( 'loadend', this, onloadagivend );
+
+    map.addLayer( agiv_layer );
+    agiv_layer.setVisibility( true );
+    /* Enable highlighting  */
+    map.addControl( highlightagiv );
+    highlightagiv.activate();
+
+    /* Overpass style */
+    var overpass_styled = new OpenLayers.Style( {
+        fillColor: "red",
+        fillOpacity: 0.6,
+        fontColor: "#000000",
+        fontWeight: "light",
+        pointRadius: 8,
+        fontSize: "11px",
+        strokeColor: "#ff9963",
+        strokeWidth: 3,
+        //externalGraphic: "${image_url}",
+        // externalGraphic: "images/entrs.png",
+        //externalGraphic: "http://www2.synctrace.com/images/entrs.png",
+        pointerEvents: "all",
+        //graphicName: 'star',
+        labelOutlineColor: "white",
+        labelOutlineWidth: 8,
+        labelAlign: "cm",
+        cursor: "pointer",
+        fontFamily: "sans-serif"
+        //fontFamily: "Courier New, monospace"
+    } );
+
+    /* Overpass select hover style */
+    var overpass_temp_styled = new OpenLayers.Style( {
+            fillColor: "red",
+            fontColor: "#000000",
+            fontWeight: "normal",
+            pointRadius: 8,
+            fontSize: "11px",
+            strokeColor: "#ff9933",
+            strokeWidth: 2,
+            // externalGraphic: null,
+            pointerEvents: "all",
+            fillOpacity: 0.3,
+            //label : "${getNumber}",
+            labelOutlineColor: "white",
+            labelAlign: "rb",
+            labelOutlineWidth: 8,
+            cursor: "pointer",
+            fontFamily: "sans-serif"
+            //fontFamily: "Courier New, monospace"
+        }
+        /*
+        , {
+            context: {
+              getLabel: function(feature) {
+                return feature.properties.tags.source;
+              },
+              getNumber: function(feature) {
+                return feature.properties.tags.building;
+              }
+            }
+          }
+        */
+    );
+
+    var overpass_style = new OpenLayers.StyleMap( {
+        'default': overpass_styled,
+        'temporary': overpass_temp_styled
+    } );
+
+    /* Style the halte layer acc to res */
+    overpass_style.styles[ 'default' ].addRules( [
+        new OpenLayers.Rule( {
+            maxScaleDenominator: 60000000,
+            minScaleDenominator: 215000,
+            symbolizer: {
+                fillColor: "red",
+                fillOpacity: 0.6,
+                fontColor: "#000000",
+                fontWeight: "light",
+                pointRadius: 2,
+                fontSize: "11px",
+                strokeColor: "#ff9963",
+                strokeWidth: 1,
+                pointerEvents: "all",
+                labelOutlineColor: "white",
+                labelOutlineWidth: 8,
+                labelAlign: "cm",
+                cursor: "pointer",
+                fontFamily: "sans-serif"
+            }
+        } ),
+        new OpenLayers.Rule( {
+            maxScaleDenominator: 215000,
+            minScaleDenominator: 27000,
+            symbolizer: {
+                fillColor: "red",
+                fillOpacity: 0.6,
+                fontColor: "#000000",
+                fontWeight: "light",
+                pointRadius: 4,
+                fontSize: "11px",
+                strokeColor: "#ff9963",
+                strokeWidth: 2,
+                pointerEvents: "all",
+                labelOutlineColor: "white",
+                labelOutlineWidth: 8,
+                labelAlign: "cm",
+                cursor: "pointer",
+                fontFamily: "sans-serif"
+
+            }
+        } ),
+        new OpenLayers.Rule( {
+            maxScaleDenominator: 27000,
+            minScaleDenominator: 3384,
+            symbolizer: {
+                fillColor: "red",
+                fillOpacity: 0.6,
+                fontColor: "#000000",
+                fontWeight: "light",
+                pointRadius: 10,
+                fontSize: "11px",
+                strokeColor: "#ff9963",
+                strokeWidth: 3,
+                pointerEvents: "all",
+                labelOutlineColor: "white",
+                labelOutlineWidth: 8,
+                labelAlign: "cm",
+                cursor: "pointer",
+                fontFamily: "sans-serif"
+
+            }
+        } ),
+        new OpenLayers.Rule( {
+            maxScaleDenominator: 3384,
+            minScaleDenominator: 1,
+            symbolizer: {
+                fillColor: "red",
+                fillOpacity: 0.6,
+                fontColor: "#000000",
+                fontWeight: "light",
+                pointRadius: 10,
+                fontSize: "11px",
+                strokeColor: "#ff9963",
+                strokeWidth: 3,
+                //label : "${source}",
+                labelAlign: "cm",
+                //labelAlign: "cm",
+                pointerEvents: "all",
+                labelOutlineColor: "white",
+                labelOutlineWidth: 8,
+                cursor: "pointer",
+                fontFamily: "sans-serif"
+            }
+        } )
+    ] );
+
+    function onloadagivend( evt ) {
+        // isvecup = null; Always do this now
+        isagivup = null;
+        if ( isagivup == null || isagivup == undefined ) {
+            // if(stuff !== null && stuff !== undefined) 
+            // console.log(poilayer);
+            $( '#cntain' ).css( "width", 'auto' );
+            $( '#contentfilters' ).empty();
+            $( '#contentfilters' ).css( "float", 'right' );
+            $( '#contentfilters' ).append( '<fieldset id="pset" style="display: inline-block; height: 56px;">' );
+            $( '#pset' ).append( '<legend class="fright">Street filter</legend>' );
+            $( '#pset' ).append( '<select id="seltagid" name="tagid" style="width:100%;">' );
+            $( '#seltagid' ).append( new Option( '*', 'None' ) );
+            //stuff = vector_layer.features;
+            //addr:street
+            var streets = {};
+
+            $.each( vector_layer.features, function( i, item ) {
+                if ( item.attributes[ 'addr:street' ] ) {
+                    streets[ item.attributes[ 'addr:street' ] ] = 1;
+                }
+            } );
+
+            var keys = [];
+            for ( var key in streets ) {
+                if ( streets.hasOwnProperty( key ) ) {
+                    keys.push( key );
+                }
+            }
+            keys.sort();
+
+            //console.log(keys);
+            $.each( keys, function( i, item ) {
+                //$('#seltagid').append(new Option(item.['addr:street'], item.asdfsadf ));
+                $( '#seltagid' ).append( new Option( item, item ) );
+            } );
+            // $('#pset').append('<div id="gicon"></div>');
+
+            $( '#contentfilters' ).append( '</fieldset>' );
+
+            $( '#seltagid' ).change( function() {
+                //streetStrategy.setFilter(null);
+                // vector_layer.refresh();
+                var filterstring = $( '#seltagid' ).val();
+                var propertysearch = 'addr:street';
+
+                if ( filterstring == 'None' ) {
+                    filterstring = ''
+                }
+
+                var myfilter = new OpenLayers.Filter.Comparison( {
+                    type: OpenLayers.Filter.Comparison.LIKE,
+                    // property: "imei",
+                    property: propertysearch,
+                    value: filterstring
+                } );
+
+                //console.log(filterstring);
+                if ( filterstring.length <= 0 ) {
+                    streetStrategy.setFilter( null );
+                } else {
+                    streetStrategy.setFilter( myfilter );
+                }
+                vector_layer.refresh();
+
+                var bounds = vector_layer.getDataExtent();
+                //map.getZoom()
+
+                if ( bounds !== null && bounds !== undefined ) {
+                    map.panTo( bounds.getCenterLonLat() );
+                    //map.zoomToExtent(bounds, true);
+                }
+            } );
+
+            // The building filter
+            $( '#contentfilters' ).append( '<fieldset id="bset" style="width: 160px; display: inline-block; height: 56px;">' );
+            $( '#bset' ).append( '<legend class="fright">Building filter</legend>' );
+            $( '#bset' ).append( '<select id="selbtype" name="tagid" style="width:100%;">' );
+            $( '#selbtype' ).append( new Option( '*', 'None' ) );
+            //stuff = vector_layer.features;
+            //addr:street
+            var buildings = {};
+
+            $.each( vector_layer.features, function( i, item ) {
+                //console.log(item.attributes['building']);
+                if ( item.attributes[ 'building' ] ) {
+                    buildings[ item.attributes[ 'building' ] ] = item.attributes[ 'building' ];
+                }
+            } );
+
+            var keys = [];
+            for ( var key in buildings ) {
+                if ( buildings.hasOwnProperty( key ) ) {
+                    keys.push( key );
+                }
+            }
+            keys.sort();
+
+            // console.log(keys);
+            $.each( keys, function( i, item ) {
+                //$('#selbtype').append(new Option(item.['addr:street'], item.asdfsadf ));
+                $( '#selbtype' ).append( new Option( item, item ) );
+            } );
+            // $('#bset').append('<div id="gicon"></div>');
+
+            $( '#contentfilters' ).append( '</fieldset>' );
+
+            $( '#selbtype' ).change( function() {
+                //buildingStrategy.setFilter(null);
+                // vector_layer.refresh();
+                var filterstring = $( '#selbtype' ).val();
+                var propertysearch = 'building';
+
+                if ( filterstring == 'None' ) {
+                    filterstring = ''
+                }
+
+                var myfilter = new OpenLayers.Filter.Comparison( {
+                    type: OpenLayers.Filter.Comparison.LIKE,
+                    // property: "imei",
+                    property: propertysearch,
+                    value: filterstring
+                } );
+
+                //console.log(filterstring);
+                if ( filterstring.length <= 0 ) {
+                    buildingStrategy.setFilter( null );
+                } else {
+                    buildingStrategy.setFilter( myfilter );
+                }
+                vector_layer.refresh();
+
+                var bounds = vector_layer.getDataExtent();
+
+                if ( bounds !== null && bounds !== undefined ) {
+                    map.panTo( bounds.getCenterLonLat() );
+                    //map.zoomToExtent(bounds, true);
+                }
+            } );
+            //console.log(poilayer.features);
+            isvecup = true;
+        }
+    }
+} );

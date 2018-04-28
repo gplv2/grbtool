84d33bcf32746862ab0f8b969abfac8d7c263dbd
Glenn Plas <glenn@byte-consult.be>
2018-04-28 15:00:03 +0000

    ssfsdf

--- public/js/grblayers.js
+++ public/js/grblayers.js
@@ -228,7 +228,7 @@ function initmap() {
         //zoomOffset: 9, resolutions: [152.87405654907226, 76.43702827453613, 38.218514137268066, 19.109257068634033, 9.554628534317017, 4.777314267158508, 2.388657133579254, 1.194328566789627, 0.5971642833948135],
         //zoomOffset: 10, resolutions: [76.43702827453613, 38.218514137268066, 19.109257068634033, 9.554628534317017, 4.777314267158508, 2.388657133579254, 1.194328566789627, 0.5971642833948135],
         protocol: new OpenLayers.Protocol.HTTP( {
-            url: "https://data.grbosm.site/grb?",
+            url: "https://betadata.grbosm.site/grb?",
             format: new OpenLayers.Format.GeoJSON( {
                 extractAttributes: true
             } )

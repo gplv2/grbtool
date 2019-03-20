e336c6350158ac02152887830c980a0b6920fd41
Glenn Plas <glenn@byte-consult.be>
2019-03-20 11:51:26 +0100

    add map resize function

--- public/js/start.js
+++ public/js/start.js
@@ -7,12 +7,12 @@
 var my_history = new Object();
 var start_time = 0;
 
-/*
 $(window).resize(function () {
-    var canvaswidth=$('#map-wrap').parent().css('width');
-    $('#map-wrap').css("width", canvaswidth);
+    var canvaswidth=$('#map').parent().css('width');
+    var w = $( "#map" ).width();
+    $( '#map' ).css( "height", w * 4 / 8.5 );
+    //$('#map-wrap').css("width", canvaswidth);
 }
-*/
 
 /* Here the map size is getting set? not sure now */
 var canvaswidth = $( '#map' ).parent().css( 'width' );

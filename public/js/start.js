88067f31bc2084af44eb4d8fa93ee8dd2c253faa
Glenn Plas <glenn@byte-consult.be>
2019-03-20 21:09:04 +0100

    add inline comments

--- public/js/start.js
+++ public/js/start.js
@@ -7,13 +7,14 @@
 var my_history = new Object();
 var start_time = 0;
 
+/* Here a map resize is getting handled */
 $( window ).resize( function() {
     var canvaswidth = $( '#map' ).parent().css( 'width' );
     var w = $( "#map" ).width();
     $( '#map-wrap' ).css( "height", w * 4 / 8.5 );
 } );
 
-/* Here the map size is getting set? not sure now */
+/* Here the map size is getting initially set */
 var canvaswidth = $( '#map' ).parent().css( 'width' );
 var w = $( "#map" ).width();
 $( '#map-wrap' ).css( "height", w * 4 / 8.5 );

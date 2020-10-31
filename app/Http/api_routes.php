13b130737eb8d261665c7f002a92ba3a08b78202
Glenn Plas <glenn@byte-consult.be>
2020-10-31 03:16:49 +0000

    adding authentication verification before export

--- app/Http/api_routes.php
+++ app/Http/api_routes.php
@@ -7,10 +7,11 @@ $api->version('v1', function ($api) {
     $api->post('auth/signup', 'App\Api\V1\Controllers\AuthController@signup');
     $api->post('auth/recovery', 'App\Api\V1\Controllers\AuthController@recovery');
     $api->post('auth/reset', 'App\Api\V1\Controllers\AuthController@reset');
+    $api->post('auth/verify', 'App\Api\V1\Controllers\AuthController@verify');
 
-	// example of protected route
-	$api->get('protected', ['middleware' => ['api.auth'], function () {
-		return \App\User::all();
+    // example of protected route
+    $api->get('protected', ['middleware' => ['api.auth'], function () {
+        return \App\User::all();
     }]);
 
    $api->group(['middleware' => 'api.auth'], function ($api) {
@@ -21,10 +22,10 @@ $api->version('v1', function ($api) {
       $api->get('option', 'App\Api\V1\Controllers\OptionController@index');
    });
 
-	// example of free route
-	$api->get('free', function() {
-		return \App\User::all();
-	});
+    // example of free route
+    $api->get('free', function() {
+        return \App\User::all();
+    });
 
     $api->get('status', ['middleware' => ['api.auth'], function () {
         // We need to trap this as cli stuff does not have JWT token inside. weird framework behavior
